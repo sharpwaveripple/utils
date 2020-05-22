d6179f6e68bed375b7187b377d1dd45c4ebc2c87
Jonathan Tay <jonathantay193@gmail.com>
2020-05-22 02:59:52 -0700

    added extra source

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -105,3 +105,5 @@ sudo make install
 git clone https://github.com/sharpwaveripple/.doom.d.git ~/.doom.d
 git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
 ~/.emacs.d/bin/doom install
+
+source ~/.bashrc
